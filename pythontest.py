3c433a749eaa45f06ab7a15279db48711d1e8013
Etrenix <ryukuzui@gmail.com>
2019-08-05 17:22:34 +0800

    db test

--- pythontest.py
+++ pythontest.py
@@ -1,6 +1,37 @@
 from flask import Flask, render_template
+import mysql.connector
+from mysql.connector import Error
+string = ''
+try:
+    connection = mysql.connector.connect(host=thehost,
+                             database=thedatabase,
+                              user=theuser,
+                             password=thepassword)
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL database... MySQL Server version on ",db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print ("Your connected to - ", record)
+        
+        mycursor = connection.cursor()
+        mycursor.execute("SELECT * FROM groceries WHERE id = 1")
+        string = mycursor.fetchall()
+        
+
+except Error as e :
+    print ("Error while connecting to MySQL", e)
+
+
+finally:
+    #closing database connection.
+    if(connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "<h1>Hello World, Jinbei is not working :(</h1>"
+    return string
