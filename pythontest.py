0d34dcc9182192da62ef8f4e508c867f7d5cdf7d
Etrenix <ryukuzui@gmail.com>
2019-08-05 17:55:21 +0800

    bug fix: didn't use os.environ.get for keys

--- pythontest.py
+++ pythontest.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 import mysql.connector
 from mysql.connector import Error
+import os
 string = ''
 try:
-    connection = mysql.connector.connect(host=thehost,
-                             database=thedatabase,
-                              user=theuser,
-                             password=thepassword)
+    connection = mysql.connector.connect(host=os.environ.get('thehost'),
+                             database=os.environ.get('thedatabase'),
+                              user=os.environ.get('theuser'),
+                             password=os.environ.get('thepassword'))
     if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL database... MySQL Server version on ",db_Info)
